50ee258b02d9372671012e7803939d8a65b40f11
yanghongyi <469945613@qq.com>
2019-12-24 11:47:57 +0800

    init

---
+++ empty_server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+./down.sh
+rm -rf /root/dst/data/DoNotStarveTogether/Cluster_1/Master/save
+rm -rf /root/dst/data/DoNotStarveTogether/Cluster_1/Master/backup
+rm -rf /root/dst/data/DoNotStarveTogether/Cluster_1/Master/*.txt
+
+rm -rf /root/dst/data/DoNotStarveTogether/Cluster_1/Caves/save
+rm -rf /root/dst/data/DoNotStarveTogether/Cluster_1/Caves/backup
+rm -rf /root/dst/data/DoNotStarveTogether/Cluster_1/Caves/*.txt
---
+++ install_docker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+				  
+sudo yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+  
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+	
+sudo yum -y install docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo systemctl enable docker
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
